66985cb74e3193a6ff24ae271ebb767d290e6283
Lydeon <leonlee190@gmail.com>
2017-11-29 05:39:37 -0800

    Update DiamondFinder.h
    header comment

--- DiamondFinder.h
+++ DiamondFinder.h
@@ -1,3 +1,9 @@
+/****************************************************
+  * Programmer: James Camarillo Ricks
+  * File: DiamondFinder.h
+  * Description: Check if the player has found "Royal Diamond" item
+  **************************************************/
+
 //Actual Code Starts Here
 #include <stdio.h>
 #include <stdlib.h>
